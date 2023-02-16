71d2eac163707624402ebeb4514cdc8cf7bd16ff
UR4N0-235 <matheus.fernandes92.235@protonmail.com>
2023-02-16 16:51:45 -0300

    fix(hilt): specified androidEntryPoint

--- app/src/main/java/com/ur4n0/sapatariafernandes/MainActivity.kt
+++ app/src/main/java/com/ur4n0/sapatariafernandes/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.ur4n0.sapatariafernandes.ui.theme.SapatariaFernandesTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
